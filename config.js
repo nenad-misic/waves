56fdfc3812a43266ed0e0412c827f0f5561b067d
nenad.misic <nenad.misic@typhoon-hil.com>
2021-08-06 15:28:16 +0200

    Mf

---
+++ config.js
@@ -0,0 +1,7 @@
+// Iteration length is 9 seconds
+// Number of iterations can be float, for example 0.5
+// In that case, color and speed will change after 4.5 seconds
+var change_color_and_speed_after_iteration = 1
+
+var colors = ['#316777', '#313878', '#593177']
+var speeds = [1, 3, 2]
