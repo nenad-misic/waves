0c617523d0b618070a7a2ee0d1238aae86949e74
nenad.misic <nenad.misic@typhoon-hil.com>
2021-08-09 12:32:22 +0200

    Initial commit

--- config.js
+++ config.js
@@ -3,5 +3,9 @@
 // In that case, color and speed will change after 4.5 seconds
 var change_color_and_speed_after_iteration = 1
 
-var colors = ['#316777', '#313878', '#593177']
-var speeds = [1, 3, 2]
+var colors = ['#313878', '#000000', '#FF0000', '#00FF00', '#0000FF', "#FFFFFF"]
+var speeds = [1, 3, 2, 0.5, 4, 20]
+
+var fps = 40;
+
+var color_shift_duration = 5
